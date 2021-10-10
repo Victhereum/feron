ef497fe04746906b4bdf3d48df7f2f1701759b85
FeronAuto <89782592+FeronAuto@users.noreply.github.com>
2021-10-10 14:58:38 +0100

    Changed DEBUG to True
    Changed DEBUG to True in order to check admin site errors

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["*", "feronauto.com","www.feronauto.com"]
 # DEBUG = os.getenv("DEBUG", "True") == "False"
-DEBUG = False
+DEBUG = True
 # DATABASES
 # ------------------------------------------------------------------------------
 if os.getenv("DATABASE_URL", "") != "":
